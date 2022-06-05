4a3a176d9debd6e4ead0e7d6b192d88f7bd3901f
Jair Barcasnegras <jbarcasnegras@juridigo.com>
2022-06-05 04:55:04 -0500

    feat: add cadr manamegemt routes

--- pasarela/src/infrastructure/entry-points/express/routes/index.ts
+++ pasarela/src/infrastructure/entry-points/express/routes/index.ts
@@ -2,12 +2,14 @@ import {Router} from "express";
 import {APP_VARIABLES} from "../../../../common/helpers/initial.config";
 import axios from "axios";
 import {bankRouter} from "./bank.route";
+import {cardRouter} from "./card.route";
 
 
 const routes = () => {
     const router = Router();
 
     router.use("/banks", bankRouter());
+    router.use("/cards", cardRouter());
 
     return router;
 };
